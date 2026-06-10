65d86610d369cd364e93dbed28943e7f164d21cc
Xanet Zaldua <xanet2004@gmail.com>
2026-06-10 18:55:53 +0200

    clean(backend): visibility repository name inconsistency

--- backend/src/main/java/net/zalduaxa/backend/model/visibility/VisibilityRepository.java
+++ backend/src/main/java/net/zalduaxa/backend/model/visibility/VisibilityRepository.java
@@ -3,5 +3,5 @@ package net.zalduaxa.backend.model.visibility;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VisibilityRepository extends JpaRepository<Visibility, Integer> {
-    Visibility findByCode(String code);
+    Visibility findByName(String name);
 }
